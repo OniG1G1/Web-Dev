92b221db9ae837f03d665fd10a596705baa2908d
Gigi <gianluigi.petrosillo@yahoo.it>
2025-07-30 18:22:46 +0200

    Refactor server logic using handler class and factory pattern for cleaner routing

---
+++ helloWorldNode_2/router.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { DateHandler } = require('./handlerFactory');
+
+// define route handlers using the factory
+const routes = {
+    '/': new DateHandler(d => d, 'Today is '),
+    '/index.html': new DateHandler(d => d, 'Today is '),
+    '/now': new DateHandler(d => d, 'Today is '),
+    '/yesterday': new DateHandler(d => {
+        d.setDate(d.getDate() - 1);
+        return d;
+    }, 'Yesterday was '),
+    '/tomorrow': new DateHandler(d => {
+        d.setDate(d.getDate() + 1);
+        return d;
+    }, 'Tomorrow will be')
+};
+
+function handle(req, res) {
+    const handler = routes[req.url];
+    if (handler) {
+        handler.handle(req, res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 - Page not found');
+    }
+}
+
+module.exports = { handle };
--- helloWorldNode_2/server.js
+++ helloWorldNode_2/server.js
@@ -1,67 +1,8 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const { handle } = require('./router');
 
 const server = http.createServer((req, res) => {
-    /*
-    const url = req.url;
-
-    if (url === '/' || url === '/index.html' || url === '/now') {
-        servePage(new Date());
-    } else if (url === '/yesterday') {
-        const yesterday = new Date();
-        yesterday.setDate(yesterday.getDate() - 1);
-        servePage(yesterday);
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 - Page not found');
-    }
-    */
-    const url = req.url;
-    const date = new Date();
-    let dateMsg;
-
-    if (url !== '/' && url !== '/index.html' && url !== '/now' && url !== '/yesterday' && url !== '/tomorrow') {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 - Page not found');
-        return;
-    }
-
-    switch (url) {
-        case '/yesterday':
-            date.setDate(date.getDate() - 1);
-            dateMsg = "Yesterday was " + getWeekday(date.getDay()) + ".";
-            break;
-        case '/tomorrow':
-            date.setDate(date.getDate() + 1);
-            dateMsg = "Tomorrow will be " + getWeekday(date.getDay()) + ".";
-            break;
-        default:
-            dateMsg = "Today is " + getWeekday(date.getDay());
-    }
-
-    servePage(dateMsg)
-
-    // helper function to serve HTML with the given date
-    function servePage(datemsg) {
-        fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Error loading HTML file');
-                return;
-            }
-
-            const pageWithDate = data.replace('{{dateMessage}}', datemsg);
-
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(pageWithDate);
-        });
-    }
-
-    function getWeekday(numDay) {
-        const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        return weekday[numDay];
-    }
+    handle(req, res);
 });
 
 server.listen(3000, () => {
