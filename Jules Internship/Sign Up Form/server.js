e963e27d9d383ff33f3fd1ca6cd28366c82cda03
Gigi <gianluigi.petrosillo@yahoo.it>
2025-08-13 16:43:52 +0200

    Refactor signup flow to cleaner MVC structure with password validation

--- Jules Internship/Sign Up Form/server.js
+++ Jules Internship/Sign Up Form/server.js
@@ -1,78 +1,27 @@
 const http = require('http');
-const fs = require('fs');
 const path = require('path');
-const querystring = require('querystring');
+const fs = require('fs');
+const { serveIndex, handleSignup } = require('./controllers/UserController');
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/' || url === '/index.html') {
-    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
+  if (req.url === '/' || req.url === '/index.html') {
+    serveIndex(res);
+  }
+  else if (req.url === '/signup' && req.method === 'POST') {
+    handleSignup(req, res);
+  }
+  else if (req.url.startsWith('/css/')) {
+    const filePath = path.join(__dirname, 'public', req.url);
+    fs.readFile(filePath, (err, data) => {
       if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Could not load form.');
+        res.writeHead(404);
+        res.end('Not Found');
         return;
       }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.writeHead(200, { 'Content-Type': 'text/css' });
       res.end(data);
     });
   }
-
-  else if (url === '/signup' && method === 'POST') {
-    let body = '';
-
-    req.on('data', chunk => {
-      body += chunk.toString();
-    });
-
-    req.on('end', () => {
-      const { username, password } = querystring.parse(body);
-
-      // Validation
-      const hasCapital = /[A-Z]/.test(password);
-      const hasLetter = /[a-zA-Z]/.test(password);
-      const hasNumber = /[0-9]/.test(password);
-      const isValid = username.trim() !== '' && hasCapital && hasLetter && hasNumber;
-
-      if (!isValid) {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.end('Invalid username or password. Password must include at least 1 capital letter, a number, and a letter.');
-        return;
-      }
-
-      const userData = { username, password };
-
-      const usersPath = path.join(__dirname, 'users.json');
-
-      // Save to file
-      fs.readFile(usersPath, 'utf8', (err, fileData) => {
-        let users = [];
-
-        if (!err && fileData) {
-          try {
-            users = JSON.parse(fileData);
-          } catch (e) {
-            users = [];
-          }
-        }
-
-        users.push(userData);
-
-        fs.writeFile(usersPath, JSON.stringify(users, null, 2), err => {
-          if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Could not save user data.');
-            return;
-          }
-
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end('Signup successful!');
-        });
-      });
-    });
-  }
-
   else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 - Not found');
